61df04b0320467f9b63b5b3b721502060a5bb1d1
MateusVergennes <mateusalpi@hotmail.com>
2023-10-06 14:39:27 -0400

    screen votation and utilities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { Route, Routes } from "react-router-dom";
 
-import {query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from 'firebase/firestore'
+import {query, collection, onSnapshot} from 'firebase/firestore'
 import {db} from './firebase'
 
 import Home from './components/Home'
@@ -18,7 +18,7 @@ function App() {
   const [cpfs, setCpfs] = useState([])
   const [fetchCPFsOnButtonClicked, setFetchCPFsOnButtonClicked] = useState(false);
 
-  //Read todo from firebase
+  //Read cpf from firebase
   const fetchCPFs = ()=> {
     const q = query(collection(db, 'cpfs'))
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -39,7 +39,6 @@ function App() {
   }, [fetchCPFsOnButtonClicked]);
 
   const readData = (cpf) => {
-    console.log(`Reading data for ${cpf}`)
     const cpfsArray = cpfs.map((item) => item.cpf);
     
     //para checar se o cpf do formulario de home inserido pelo usuario esta no banco de dados
@@ -51,7 +50,7 @@ function App() {
         <h1 className='text-center text-3xl font-bold p-4'>Votação de Funcionários</h1>
         <Routes>
           <Route path='/' element={<Home readData = {readData} setFetchCPFsOnButtonClicked={setFetchCPFsOnButtonClicked} />} />
-          <Route path='/Votar' element={<Votar />} />
+          <Route path='/Votar' element={<Votar cpfs={cpfs} fetchCPFs={fetchCPFs} />} />
           <Route path='/Resultados' element={<Resultados />} />
           <Route path='/Admin' element={<Admin />} />
         </Routes>
